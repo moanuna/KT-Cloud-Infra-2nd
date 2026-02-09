8fbb1587c81156eaceba61cc2f92c38ec4110ea4
seung yeon <137742302+moanuna@users.noreply.github.com>
2026-02-09 12:56:00 +0900

    feat : 26-02-09 테라폼 s3 실습

---
+++ ec2-instance-demo/s3.tf
@@ -0,0 +1,26 @@
+resource "aws_s3_bucket" "data" {
+  bucket = "my-unique-bucket-name-2026"
+
+  tags = {
+    Name = "DataBucket"
+    Environment = "Dev"
+  }
+}
+
+# 버킷 버전 관리 활성화
+resource "aws_s3_bucket_versioning" "data_versioning" {
+  bucket = var.bucket_name
+
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "data_block" {
+  bucket = aws_s3_bucket.data.id
+
+  block_public_acls = true
+  block_public_policy = true
+  ignore_public_acls = true
+  restrict_public_buckets = true
+}
---
+++ ec2-instance-demo/variables.tf
@@ -0,0 +1,4 @@
+variable "bucket_name" {
+  description = "S3 버킷 이름(전역고유)"
+  type = string
+}
